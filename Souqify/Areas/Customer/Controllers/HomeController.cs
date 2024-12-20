ed291cfa1b1014e76140b6a74745c6e82a721c38
othman <othmanelrabiey@gmail.com>
2024-12-20 21:24:08 +0200

    Home and details page

--- Souqify/Areas/Customer/Controllers/HomeController.cs
+++ Souqify/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Souqify.DataAccess.Repository.IRepository;
 using Souqify.Models;
 
 namespace Souqify.Areas.Customer.Controllers
@@ -8,15 +9,26 @@ namespace Souqify.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+
+            return View(productList);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Product product = _unitOfWork.Product.GetFirstOrDefault(p => p.Id == id, includeProperties: "Category");
+
+            return View(product);
         }
 
         public IActionResult Privacy()

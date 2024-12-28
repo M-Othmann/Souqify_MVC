b4cdb6ce8dc7d2fd92d69bb7dcbe908a18d6ca22
othman <othmanelrabiey@gmail.com>
2024-12-28 12:39:23 +0200

    make status selected active

--- Souqify.Utilities/SD.cs
+++ Souqify.Utilities/SD.cs
@@ -27,5 +27,9 @@ namespace Souqify.Utilities
         public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
         public const string PaymentStatusRejected = "Rejected";
 
+
+
+        public const string SessionCart = "SessionShoppingCart";
+
     }
 }
---
+++ Souqify/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Souqify.DataAccess.Repository.IRepository;
+using Souqify.Models;
+
+namespace Souqify.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API Calls
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader
+               .GetAll(includeProperties: "ApplicationUser")
+               .ToList();
+
+            return Json(new { data = objOrderHeaders });
+        }
+
+        #endregion
+    }
+}

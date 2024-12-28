5597cc079857af25fad46348065125760d567989
othman <othmanelrabiey@gmail.com>
2024-12-28 16:46:54 +0200

    order management

--- Souqify/Areas/Admin/Controllers/OrderController.cs
+++ Souqify/Areas/Admin/Controllers/OrderController.cs
@@ -1,17 +1,24 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Souqify.DataAccess.Repository.IRepository;
 using Souqify.Models;
+using Souqify.Models.ViewModels;
 using Souqify.Utilities;
+using Stripe;
+using Stripe.Checkout;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace Souqify.Areas.Admin.Controllers
 {
     [Area("admin")]
+    [Authorize]
     public class OrderController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-
+        [BindProperty]
+        public OrderVM orderVm { get; set; }
         public OrderController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
@@ -22,15 +29,205 @@ namespace Souqify.Areas.Admin.Controllers
             return View();
         }
 
+        public IActionResult Details(int orderId)
+        {
+            orderVm = new()
+            {
+                OrderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(oh => oh.Id == orderId, includeProperties: "ApplicationUser"),
+                OrderDetail = _unitOfWork.OrderDetail.GetAll(od => od.OrderHeaderId == orderId, includeProperties: "Product")
+            };
+            return View(orderVm);
+        }
 
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        public IActionResult UpdateOrderDetail()
+        {
+            var orderHeaderFromDb = _unitOfWork.OrderHeader.GetFirstOrDefault(o => o.Id == orderVm.OrderHeader.Id);
+
+
+            orderHeaderFromDb.Name = orderVm.OrderHeader.Name;
+            orderHeaderFromDb.PhoneNumber = orderVm.OrderHeader.PhoneNumber;
+            orderHeaderFromDb.StreetAddress = orderVm.OrderHeader.StreetAddress;
+            orderHeaderFromDb.City = orderVm.OrderHeader.City;
+            orderHeaderFromDb.State = orderVm.OrderHeader.State;
+            orderHeaderFromDb.PostalCode = orderVm.OrderHeader.PostalCode;
+
+            if (!string.IsNullOrEmpty(orderVm.OrderHeader.Carrier))
+                orderHeaderFromDb.Carrier = orderVm.OrderHeader.Carrier;
+
+            if (!string.IsNullOrEmpty(orderVm.OrderHeader.TrackingNumber))
+                orderHeaderFromDb.Carrier = orderVm.OrderHeader.TrackingNumber;
+
+            _unitOfWork.OrderHeader.update(orderHeaderFromDb);
+            _unitOfWork.Save();
+
+            TempData["Success"] = "Order Details Updated Successfully";
+
+            return RedirectToAction(nameof(Details), new { orderId = orderHeaderFromDb.Id });
+        }
+
+
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        public IActionResult StartProcessing()
+        {
+            _unitOfWork.OrderHeader.UpdateStatus(orderVm.OrderHeader.Id, SD.StatusInProcess);
+            _unitOfWork.Save();
+            TempData["Success"] = "Order Status Updated Successfully";
+
+            return RedirectToAction(nameof(Details), new { orderId = orderVm.OrderHeader.Id });
+
+        }
+
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        public IActionResult ShipOrder()
+        {
+            var orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == orderVm.OrderHeader.Id);
+
+            orderHeader.TrackingNumber = orderVm.OrderHeader.TrackingNumber;
+            orderHeader.Carrier = orderVm.OrderHeader.Carrier;
+            orderHeader.OrderStatus = SD.StatusShipped;
+            orderHeader.ShippingDate = DateTime.Now;
+
+            if (orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
+            {
+                orderHeader.PaymentDueDate = DateOnly.FromDateTime(DateTime.Now.AddDays(30));
+            }
+
+
+
+            _unitOfWork.OrderHeader.update(orderHeader);
+            _unitOfWork.Save();
+            TempData["Success"] = "Order Shipped Successfully";
+
+            return RedirectToAction(nameof(Details), new { orderId = orderVm.OrderHeader.Id });
+
+        }
+
+
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        public IActionResult CancelOrder()
+        {
+            var orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == orderVm.OrderHeader.Id);
+
+            if (orderHeader.PaymentStatus == SD.PaymentStatusApproved)
+            {
+                var options = new RefundCreateOptions
+                {
+                    Reason = RefundReasons.RequestedByCustomer,
+                    PaymentIntent = orderHeader.PaymentIntentId,
+
+                };
+
+                var service = new RefundService();
+                Refund refund = service.Create(options);
+
+                _unitOfWork.OrderHeader.UpdateStatus(orderHeader.Id, SD.StatusCancelled, SD.StatusRefunded);
+            }
+            else
+            {
+                _unitOfWork.OrderHeader.UpdateStatus(orderHeader.Id, SD.StatusCancelled, SD.StatusCancelled);
+            }
+            _unitOfWork.Save();
+            TempData["Success"] = "Order Cancelled Successfully";
+
+            return RedirectToAction(nameof(Details), new { orderId = orderVm.OrderHeader.Id });
+        }
+
+
+
+
+        [HttpPost]
+        [ActionName("Details")]
+        public IActionResult Details_PAY_NOW()
+        {
+            orderVm.OrderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == orderVm.OrderHeader.Id, includeProperties: "ApplicationUser");
+
+            orderVm.OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderHeaderId == orderVm.OrderHeader.Id, includeProperties: "Product");
+
+            var domain = "https://localhost:44314/";
+            //regular user we need to capture payment stripe logic
+            var options = new Stripe.Checkout.SessionCreateOptions
+            {
+                SuccessUrl = domain + $"admin/order/PaymentConfirmation?orderHeaderId={orderVm.OrderHeader.Id}",
+                CancelUrl = domain + $"admin/order/details?orderId={orderVm.OrderHeader.Id}",
+                LineItems = new List<Stripe.Checkout.SessionLineItemOptions>(),
+                Mode = "payment",
+            };
+
+            foreach (var item in orderVm.OrderDetail)
+            {
+                var sessionLineItem = new SessionLineItemOptions
+                {
+                    PriceData = new SessionLineItemPriceDataOptions
+                    {
+                        UnitAmount = (long)(item.Price * 100), //$20.50 => 2050
+                        Currency = "usd",
+                        ProductData = new SessionLineItemPriceDataProductDataOptions
+                        {
+                            Name = item.Product.Title
+                        }
+                    },
+                    Quantity = item.Count
+                };
+                options.LineItems.Add(sessionLineItem);
+            }
+            var service = new Stripe.Checkout.SessionService();
+            Session session = service.Create(options);
+            _unitOfWork.OrderHeader.UpdateStripePaymentId(orderVm.OrderHeader.Id, session.Id, session.PaymentIntentId);
+            _unitOfWork.Save();
+
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303);
+
+        }
+
+
+        public IActionResult PaymentConfirmation(int orderHeaderId)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(o => o.Id == orderHeaderId);
+            if (orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
+            {
+                //order made by company
+                var service = new SessionService();
+
+                Session session = service.Get(orderHeader.SessionId);
+
+                if (session.PaymentStatus.ToLower() == "paid")
+                {
+                    _unitOfWork.OrderHeader.UpdateStripePaymentId(orderHeaderId, session.Id, session.PaymentIntentId);
+                    _unitOfWork.OrderHeader.UpdateStatus(orderHeaderId, orderHeader.OrderStatus, SD.PaymentStatusApproved);
+                    _unitOfWork.Save();
+                }
+            }
+
+
+
+            return View(orderHeaderId);
+        }
         #region API Calls
 
         [HttpGet]
         public IActionResult GetAll(string status)
         {
-            IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader
-               .GetAll(includeProperties: "ApplicationUser")
-               .ToList();
+            IEnumerable<OrderHeader> objOrderHeaders;
+
+            if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+            {
+                objOrderHeaders = _unitOfWork.OrderHeader
+                .GetAll(includeProperties: "ApplicationUser")
+                .ToList();
+            }
+            else
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+                objOrderHeaders = _unitOfWork.OrderHeader.GetAll(u => u.ApplicationUserId == userId, includeProperties: "ApplicationUser");
+            }
+
 
             switch (status)
             {
